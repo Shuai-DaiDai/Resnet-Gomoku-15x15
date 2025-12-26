7217bc79f8b83ce5c73a64fef08e6059a640a7ae
Shuai-DaiDai <sddsyf@gmail.com>
2025-12-26 11:56:00 +0800

    还是报错，再试试小点

--- train.py
+++ train.py
@@ -123,10 +123,10 @@ def train():
                 new_data_received = True # 核心：标记收到了新棋谱
         
         # --- C. 神经网络参数更新 ---
-        if len(buffer) > 5120:
+        if len(buffer) > 4096:
             net.train()
             # 5090 核心：直接开启 512 或 1024 大 Batch 训练
-            batch = random.sample(buffer, 5120)
+            batch = random.sample(buffer, 4096)
             s_b, p_b, z_b = zip(*batch)
             s_t = torch.FloatTensor(np.array(s_b)).to(device)
             p_t = torch.FloatTensor(np.array(p_b)).to(device)
@@ -137,7 +137,7 @@ def train():
         else:
             # 【关键修复】如果 Buffer 不够且没有新数据进来，休眠 1 秒，防止空转
             if not new_data_received:
-                time.sleep(0.1)
+                time.sleep(1)
                 continue # 跳过本次循环，不触发下面的 i 计数和存盘
 
             # 混合精度上下文，自动分配 5090 算力
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     # 1. 提升系统允许同时打开的文件句柄数，解决 "Too many open files" 报错
     import resource
     soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
-    resource.setrlimit(resource.RLIMIT_NOFILE, (5120, hard))
+    resource.setrlimit(resource.RLIMIT_NOFILE, (4096, hard))
     
     # 2. 强制设置进程启动模式为 spawn，这是 CUDA 多进程的硬性要求
     try:
